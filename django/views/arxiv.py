9a1fdd8f55db8d30e990d2e3d899c72258b424ab
Daniel Deutsch <deudan1010@gmail.com>
2018-08-03 15:58:09 +0200

    #19 Remove unused folders according to review

--- django/views/arxiv.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from arxiv import arxiv_api
-import json
-
-
-def index(request):
-    results = arxiv_api.fetch_papers()
-    return HttpResponse(json.dumps(results, indent=4))
-

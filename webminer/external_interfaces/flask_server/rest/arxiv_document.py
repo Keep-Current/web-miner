e372f8c7960c11bd354dbf334b194caa56ef1efc
Daniel Deutsch <deudan1010@gmail.com>
2018-08-06 16:28:47 +0200

    #39 Make request arxiv a use case

--- webminer/external_interfaces/flask_server/rest/arxiv_document.py
+++ webminer/external_interfaces/flask_server/rest/arxiv_document.py
@@ -6,7 +6,7 @@ from flask import Blueprint, request, Response
 from webminer.use_cases import request_objects as req
 from webminer.use_cases import arxiv_document_use_case as uc
 from webminer.interface_adapters import response_object as res
-from webminer.interface_adapters.arxiv_parser import arxiv_repo as ar
+from webminer.use_cases.request_arxiv import arxiv_repo as ar
 from webminer.interface_adapters.serializers.json import arxiv_document_serializer as ser
 
 blueprint = Blueprint("arxiv", __name__)

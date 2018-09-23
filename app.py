1b486f1437da520862a34354272e306a20733d51
Liad Magen <liad.magen@gmail.com>
2018-09-23 13:48:03 +0200

    applying app to pylint standards

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Executes the app when running using gunicorn
+"""
+
 
 from webminer.external_interfaces.flask_server.app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()    
+    app.run()
